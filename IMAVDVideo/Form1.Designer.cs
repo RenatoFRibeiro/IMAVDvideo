dc4bbf49d535427d8fc050f016fcd7d02245ab8a
Renato Ribeiro <ribeiro.renatofernandes@gmail.com>
2020-06-19 05:19:58 +0100

    commit

---
+++ IMAVDVideo/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using Emgu.CV.UI;
+
+namespace IMAVDVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.start = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.Location = new System.Drawing.Point(12, 12);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(629, 426);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            this.cameraBox.Click += new System.EventHandler(this.cameraBox_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(647, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(129, 59);
+            this.start.TabIndex = 3;
+            this.start.Text = "Começar Deteção e Reconhecimento";
+            this.start.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(647, 173);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(129, 33);
+            this.SaveButton.TabIndex = 4;
+            this.SaveButton.Text = "Guardar Cara";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(647, 147);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(129, 20);
+            this.textName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(674, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome do rosto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.cameraBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ImageBox cameraBox;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
